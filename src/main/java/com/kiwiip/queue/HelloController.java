6569b9958edd0e0ff625396f9981c652123c19e7
sirenfei <sirenfei@users.noreply.github.com>
2020-03-29 11:57:17 +1300

    adding user to list

--- src/main/java/com/kiwiip/queue/HelloController.java
+++ src/main/java/com/kiwiip/queue/HelloController.java
@@ -1,20 +1,36 @@
 package com.kiwiip.queue;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping("/greeting")
-	public String index() {
-		return "Greetings from Spring Boot!";
-	}
+	// @RequestMapping("/greeting")
+	// public String index() {
+	// 	return "Greetings from Spring Boot!";
+	// }
 
-    @GetMapping("/")
-	public String greeting() {
-		return "index";
-    }
+    // @GetMapping("/")
+	// public String greeting() {
+	// 	return "index";
+    // }
     
+    // @MessageMapping("/news")
+    // @SendTo("/topic/news")
+    // public String broadcastNews(@Payload String message) {
+    //     return message;
+    // }
+
+    @MessageMapping("/hello")
+	@SendTo("/topic/greetings")
+	public Greeting greeting(HelloMessage message) throws Exception {
+		Thread.sleep(1000); // simulated delay
+		return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+	}
 }
--- src/main/java/com/kiwiip/queue/WebSocketConfig.java
+++ src/main/java/com/kiwiip/queue/WebSocketConfig.java
@@ -1,17 +1,26 @@
 package com.kiwiip.queue;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+// import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Bean
-    public ServerEndpointExporter serverEndpointExporter() {
-        return new ServerEndpointExporter();
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+      registry.addEndpoint("/gs-guide-websocket")
+          .setAllowedOrigins("mydomain.com").withSockJS();
+    }
+  
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config){ 
+      config.enableSimpleBroker("/topic/", "/queue/");
+      config.setApplicationDestinationPrefixes("/app");
     }
 }
